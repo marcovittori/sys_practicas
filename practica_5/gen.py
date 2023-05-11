cbcf76d45541b732d0c5d07659e4d172d7579f84
marcovittori <131837019+marcovittori@users.noreply.github.com>
2023-05-11 18:19:57 -0300

    Add files via upload

---
+++ practica_5/gen.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 20 19:28:33 2023
+
+@author: martin
+"""
+
+
+def generar_seno():
+    print("generando seno...")
+    return
+
+if __name__ == '__main__':
+    generar_seno()
+#import sys
+#print(sys.path)
